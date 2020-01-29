882203552d4f0e62fb847c7c5efe98537ea85904
Surjeet Singh <surjeet.devops@gmail.com>
2020-01-29 16:12:05 +0000

    Update app.py

--- docker_flask/app.py
+++ docker_flask/app.py
@@ -7,7 +7,7 @@ api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {'hello': 'world, How are you?'}
 
 api.add_resource(HelloWorld, '/')
 
